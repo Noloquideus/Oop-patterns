022a54697ee04041f10a71250fa690294ad95fc3
Noloquideus <daniilmanukian@gmail.com>
2023-10-29 20:06:39 +0300

    Singleton

---
+++ Singleton/Singleton.py
@@ -0,0 +1,21 @@
+class Singleton:
+    __instance = None
+
+    @staticmethod
+    def get_instance():
+        if Singleton.__instance is None:
+            Singleton()
+        return Singleton.__instance
+
+    def __init__(self):
+        if Singleton.__instance is not None:
+            raise Exception("This class is a singleton!")
+        else:
+            Singleton.__instance = self
+
+
+# Usage example
+singleton_instance1 = Singleton.get_instance()
+singleton_instance2 = Singleton.get_instance()
+
+print(singleton_instance1 is singleton_instance2)  # True
