ce2363f31fc6d1f1f0adcde02f9c44135f01ee84
Noloquideus <daniilmanukian@gmail.com>
2023-10-29 20:06:23 +0300

    object pool

---
+++ Prototype/Prototype.py
@@ -0,0 +1,27 @@
+import copy
+
+
+class Prototype:
+    def clone(self):
+        return copy.deepcopy(self)
+
+
+class ConcretePrototype(Prototype):
+    def __init__(self, data):
+        self.data = data
+
+    def __str__(self):
+        return f"Data: {self.data}"
+
+
+# Пример использования
+prototype = ConcretePrototype("Initial data")
+clone = prototype.clone()
+
+print(prototype)  # "Data: Initial data"
+print(clone)  # "Data: Initial data"
+
+clone.data = "Modified data"
+
+print(prototype)  # "Data: Initial data"
+print(clone)  # "Data: Modified data"
