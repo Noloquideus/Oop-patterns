327d7d5e8459f4b70484705a443e9087bfd7d302
Noloquideus <daniilmanukian@gmail.com>
2023-10-29 20:04:44 +0300

    builder

---
+++ Builder/builder.py
@@ -0,0 +1,33 @@
+class Car:
+    def __init__(self, model, color, engine_power):
+        self.model = model
+        self.color = color
+        self.engine_power = engine_power
+
+
+class CarBuilder:
+    def __init__(self):
+        self.model = None
+        self.color = None
+        self.engine_power = None
+
+    def set_model(self, model):
+        self.model = model
+        return self
+
+    def set_color(self, color):
+        self.color = color
+        return self
+
+    def set_engine_power(self, engine_power):
+        self.engine_power = engine_power
+        return self
+
+    def build(self):
+        return Car(self.model, self.color, self.engine_power)
+
+
+# Using the Template Builder
+car_builder = CarBuilder()
+car_builder.set_model("Sedan").set_color("Red").set_engine_power(200)
+car = car_builder.build()
