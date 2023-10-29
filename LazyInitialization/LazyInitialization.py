8d6274bd0759318084d1b7b0af4356e75de51ffc
Noloquideus <daniilmanukian@gmail.com>
2023-10-29 20:05:27 +0300

    LazyInitialization

---
+++ LazyInitialization/LazyInitialization.py
@@ -0,0 +1,21 @@
+def _initialize_expensive_data():
+    return "Expensive data"
+
+
+class LazyInitialization:
+    def __init__(self):
+        self._expensive_data = None
+
+    @property
+    def expensive_data(self):
+        if self._expensive_data is None:
+            self._expensive_data = _initialize_expensive_data()
+        return self._expensive_data
+
+
+# Usage example
+lazy_obj = LazyInitialization()
+
+print(lazy_obj.expensive_data)  # "Expensive data"
+
+print(lazy_obj.expensive_data)  # "Expensive data"
