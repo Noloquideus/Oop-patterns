27a0cbc44ef57a01b8abb0c710d723b1d02f6630
Noloquideus <daniilmanukian@gmail.com>
2023-10-29 20:06:01 +0300

    ObjectPool

---
+++ ObjectPool/ObjectPool.py
@@ -0,0 +1,21 @@
+import threading
+
+
+class ObjectPool:
+    def __init__(self, object_factory, max_size):
+        self._object_factory = object_factory
+        self._max_size = max_size
+        self._lock = threading.Lock()
+        self._pool = []
+
+    def acquire(self):
+        with self._lock:
+            if len(self._pool) > 0:
+                return self._pool.pop()
+            else:
+                return self._object_factory()
+
+    def release(self, obj):
+        with self._lock:
+            if len(self._pool) < self._max_size:
+                self._pool.append(obj)
